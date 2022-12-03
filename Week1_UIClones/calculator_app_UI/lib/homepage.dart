072606df154e7d08e274a8d27a21d1daed56942d
Moeed Irfan <moeedirfan713@gmail.com>
2022-12-03 16:24:04 +0500

    build calcultor app UI

--- Week1_UIClones/calculator_app_UI/lib/homepage.dart
+++ Week1_UIClones/calculator_app_UI/lib/homepage.dart
@@ -11,25 +11,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   // Function of creating buttons of calculator
-  Widget CalcButton(String btntxt) {
+  Widget CalcButton(String btntxt, Color color) {
     return Container(
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(10, 20, 10, 14),
-        child: ElevatedButton(
-          onPressed: () {},
-          child: Text(
-            btntxt,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 35,
-            ),
+      margin: const EdgeInsets.fromLTRB(10, 14, 10, 10),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text(
+          btntxt,
+          style: TextStyle(
+            color: color,
+            fontSize: 30,
           ),
-          style: ElevatedButton.styleFrom(
-            backgroundColor: const Color(0xff272B33),
-            padding: const EdgeInsets.all(10),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xff272B33),
+          padding: const EdgeInsets.all(10),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
           ),
         ),
       ),
@@ -45,28 +43,46 @@ class _HomePageState extends State<HomePage> {
           children: [
             Container(
               color: const Color(0xff22252D),
-              height: MediaQuery.of(context).size.height * 0.35,
-              // TODO
-              // Work on creating upperpart od container
-              // child: Row(
-              //   mainAxisAlignment: MainAxisAlignment.end,
-              //   children: const <Widget>[
-              //     Padding(
-              //       padding: EdgeInsets.all(10.0),
-              //       child: Text(
-              //         '0',
-              //         textAlign: TextAlign.left,
-              //         style: TextStyle(
-              //           fontSize: 50,
-              //           color: Colors.white,
-              //         ),
-              //       ),
-              //     ),
-              //   ],
-              // ),
+              height: MediaQuery.of(context).size.height * 0.45,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const <Widget>[
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(8, 8, 18, 8),
+                        child: Text(
+                          '23 * 80',
+                          style: TextStyle(
+                            fontSize: 50,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const <Widget>[
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(8, 8, 18, 30),
+                        child: Text(
+                          '4365',
+                          style: TextStyle(
+                            letterSpacing: 1.2,
+                            fontSize: 70,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
             Container(
-              height: MediaQuery.of(context).size.height * 0.65,
+              height: MediaQuery.of(context).size.height * 0.55,
               decoration: const BoxDecoration(
                 color: Color(0xff292D36),
                 borderRadius: BorderRadius.only(
@@ -75,53 +91,53 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               child: Padding(
-                padding: const EdgeInsets.all(12),
+                padding: const EdgeInsets.fromLTRB(0, 14, 0, 20),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        CalcButton('AC'),
-                        CalcButton('+/-'),
-                        CalcButton('%'),
-                        CalcButton('/'),
+                        CalcButton('AC', Colors.green),
+                        CalcButton('(', Colors.green),
+                        CalcButton(')', Colors.green),
+                        CalcButton('/', Colors.pink),
                       ],
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        CalcButton('7'),
-                        CalcButton('8'),
-                        CalcButton('9'),
-                        CalcButton('x'),
+                        CalcButton('7', Colors.white),
+                        CalcButton('8', Colors.white),
+                        CalcButton('9', Colors.white),
+                        CalcButton('x', Colors.pink),
                       ],
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        CalcButton('4'),
-                        CalcButton('5'),
-                        CalcButton('6'),
-                        CalcButton('-'),
+                        CalcButton('4', Colors.white),
+                        CalcButton('5', Colors.white),
+                        CalcButton('6', Colors.white),
+                        CalcButton('-', Colors.pink),
                       ],
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        CalcButton('1'),
-                        CalcButton('2'),
-                        CalcButton('3'),
-                        CalcButton('+'),
+                        CalcButton('1', Colors.white),
+                        CalcButton('2', Colors.white),
+                        CalcButton('3', Colors.white),
+                        CalcButton('+', Colors.pink),
                       ],
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        CalcButton('0'),
-                        CalcButton('0'),
-                        CalcButton('.'),
-                        CalcButton('='),
+                        CalcButton('0', Colors.white),
+                        CalcButton('%', Colors.white),
+                        CalcButton('.', Colors.white),
+                        CalcButton('=', Colors.pink),
                       ],
                     ),
                   ],
