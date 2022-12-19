1c9a83cf744f857e8fd43f5555daa98ece92300e
Moeed Irfan <moeedirfan713@gmail.com>
2022-12-19 23:38:54 +0500

    add appbar to app

--- Week2_BasicApps/todo_list_app/lib/screens/home.dart
+++ Week2_BasicApps/todo_list_app/lib/screens/home.dart
@@ -8,23 +8,31 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: tdBGColor,
-      appBar: AppBar(
-        backgroundColor: tdBGColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: const [
-            Icon(
-              Icons.menu,
-              color: tdBlack,
-              size: 30,
+      appBar: customAppBar(),
+    );
+  }
+
+  AppBar customAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('images/avatar.jpeg'),
             ),
-            // Working on appbar  
-            SizedBox(
-              height: 40,
-              width: 40,
-            ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
